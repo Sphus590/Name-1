6919ceb7907e446be44ffc29c7838fda1a23382c
Sphus590 <81660993+Sphus590@users.noreply.github.com>
2021-05-31 10:22:11 +1200

    Add files via upload

---
+++ getting user answers v1.py
@@ -0,0 +1,11 @@
+if ans == 'MS dhoni' or ans == 'ms dhoni' or ans =='A' or ans == 'a':
+        print("Correct")
+        score+=1
+        print("Your score is",score)
+    else:
+        print("Sorry this answer is incorrect the correct answer is :" )
+        if score <=0:
+            score = 0
+            print("Your score is",score)
+
+# will use this same progrm for each program
