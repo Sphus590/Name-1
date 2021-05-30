736f329cfc4b1df487f7e9cc5ea14409b4111b73
Sphus590 <81660993+Sphus590@users.noreply.github.com>
2021-05-31 10:16:27 +1200

    Add files via upload

---
+++ user_details v2.py
@@ -0,0 +1,17 @@
+print ("***************WELCOME TO THE QUIZ******************")
+#Asking for user details, name, age ,year level
+
+
+while True:  
+    name = input("Please enter your name : ")
+    if name.isalpha():
+        print("Greetings",name,"welcome to the general knowledge quiz")
+        break
+    else:
+        print("please enter name using only alphabets :)")
+
+
+
+
+
+
